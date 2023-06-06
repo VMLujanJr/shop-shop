f19771a090d20f1fc9765682084cc4190d030588
VMLujanJr <vmlujanjr@outlook.com>
2023-06-06 15:32:35 -0500

    create a global state and update files

---
+++ client/build/precache-manifest.fabd2b99893707bbead439597a57ea4a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f731f4e71ae0160adbdc5d39252099f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4087172b53383c016f1f",
+    "url": "/static/css/main.db415adc.chunk.css"
+  },
+  {
+    "revision": "4f83614d45b48e1aba37",
+    "url": "/static/js/2.3aede86e.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.3aede86e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4087172b53383c016f1f",
+    "url": "/static/js/main.a849045b.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/static/js/main.a849045b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d52f44d6fb82598fed4",
+    "url": "/static/js/runtime-main.c62b8046.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
